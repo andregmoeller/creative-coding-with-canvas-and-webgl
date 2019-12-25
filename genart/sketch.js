aaede2f53a6dee642847397f9f1826a78231a203
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2019-12-25 19:44:37 +0100

    Sets up an empty canvas

--- genart/sketch.js
+++ genart/sketch.js
@@ -1,25 +1,11 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: 'A4',
-  orientation: 'landscape',
-  units: 'cm',
-  pixelsPerInch: 300
+  dimensions: [2048, 2048]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'orange';
-    context.fillRect(0, 0, width, height);
-
-    context.beginPath();
-    context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
-    context.fillStyle = 'red';
-    context.fill();
-    context.lineWidth = width * 0.05;
-    context.strokeStyle = 'blue';
-    context.stroke();
-
   };
 };
 
