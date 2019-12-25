e1b7396970b7ffc92f1fab3eaa7f8f559b7cfd3b
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2019-12-25 21:26:20 +0100

    Changes the radius

--- genart/sketch.js
+++ genart/sketch.js
@@ -1,12 +1,17 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
 
 const settings = {
   dimensions: [2048, 2048]
 };
 
 const sketch = () => {
+  const colorCount = random.rangeFloor(2, 6);
+  const palette = random.shuffle(random.pick(palettes))
+    .slice(0, colorCount);
+
   const createGrid = () => {
     const points = [];
     const count = 30;
@@ -15,9 +20,11 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.025;
 
         points.push({
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          color: random.pick(palette),
+          radius,
           position: [u, v ]
         });
       }
@@ -34,14 +41,14 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    points.forEach(({ position, radius }) => {
+    points.forEach(({ color, position, radius }) => {
       const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
       context.arc(x, y, radius * width, 0, Math.PI * 2, false);
-      context.fillStyle = 'red';
+      context.fillStyle = color;
       context.lineWidth = 20;
       context.fill();
     }); 
