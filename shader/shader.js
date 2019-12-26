adc742213428b8a957ad1e9d9c5e31231d45296a
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2019-12-26 20:02:58 +0100

    Creates a gradient

--- shader/shader.js
+++ shader/shader.js
@@ -16,7 +16,10 @@ const frag = glsl(`
   varying vec2 vUv;
 
   void main () {
-    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
+    vec3 colorA = vec3(1.0, 0.0, 0.0);
+    vec3 colorB = vec3(0.0, 0.5, 0.0);
+
+    vec3 color = mix(colorA, colorB, vUv.x);
     gl_FragColor = vec4(color, 1.0);
   }
 `);
